3ddaaf04b741bb9c7ed8139f0cd756dd17912ea6
victorjeanpierre <103384558+victorjeanpierre@users.noreply.github.com>
2024-07-15 23:01:30 -0500

    Add files via upload

---
+++ php-api.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 16-07-2024 a las 05:47:33
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `php-api`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tm_categoria`
+--
+
+CREATE TABLE `tm_categoria` (
+  `cat_id` int(11) NOT NULL,
+  `cat_nom` varchar(250) NOT NULL,
+  `cat_obs` varchar(250) NOT NULL,
+  `est` int(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `tm_categoria`
+--
+
+INSERT INTO `tm_categoria` (`cat_id`, `cat_nom`, `cat_obs`, `est`) VALUES
+(1, 'televisores', 'observacion tv', 1),
+(2, 'refrigeradores', 'observacion refrigeradores', 1),
+(3, 'Prueba', 'prueba terrible', 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tm_categoria`
+--
+ALTER TABLE `tm_categoria`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tm_categoria`
+--
+ALTER TABLE `tm_categoria`
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
